fccc3b326854f25cbcdec55bd2847c7e505ff3e5
David Nieto <davidreynaldonieto@gmail.com>
2021-05-12 12:08:22 -0400

    modify html elements and js code to accommodate fetch data

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-let studioGhibliFilmsList = document.querySelector('ul');
+let studioGhibliFilmTitle = document.querySelector('ul');
 
 fetch('https://ghibliapi.herokuapp.com/films')
 .then(function(response){
 return response.json();
 })
 .then(async function(json){
-    for(let i=0; i<=json.length-1; i++){
+    for(let i=1; i<json.length; i++){
         listItem = document.createElement('li');
         listItem.innerHTML = '<p>' +(`${json[i].title}`)+ '</p>';
-        studioGhibliFilmsList.appendChild(listItem);
+        studioGhibliFilmTitle.appendChild(listItem);
     }
     console.log(json);
 });
@@ -16,7 +16,7 @@ return response.json();
 
 
 /*
-console.log(`The ${(json[i].name)} species tend to have the following eye color(s): ${(json[i].eye_colors)} and the following hair color(s): ${(json[i].hair_colors)}`);
+console.log(`${(json[i].title)} = the title of the film; ${(json[i].description)} = a brief description of the film; ${(json[i].release_date)} = the year the film was released`);
 */
 
 /*
