b4a7b09a076805a36996175a19cd5c990b3b8ec0
David Nieto <davidreynaldonieto@gmail.com>
2021-05-13 07:38:45 -0400

    complete html. turn in js after restarting with GET only

--- scripts.js
+++ scripts.js
@@ -1,24 +1,20 @@
-let studioGhibliFilmTitle = document.querySelector('ul');
-
-fetch('https://ghibliapi.herokuapp.com/films')
-.then(function(response){
-return response.json();
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: true,
+  pause: 'hover'
 })
-.then(async function(json){
-    for(let i=1; i<json.length; i++){
-        listItem = document.createElement('li');
-        listItem.innerHTML = '<p>' +(`${json[i].title}`)+ '</p>';
-        studioGhibliFilmTitle.appendChild(listItem);
-    }
-    console.log(json);
-});
+
+const api_url = 'https://ghibliapi.herokuapp.com/films';
+let filmTitle = document.getElementById('h1');
+
+async function getData(){
+    let response = await fetch(api_url);
+    let data = await response.json()
+    return data;
+}
+
+getData()
+.then(data => console.log(data))
 
 
-
-/*
-console.log(`${(json[i].title)} = the title of the film; ${(json[i].description)} = a brief description of the film; ${(json[i].release_date)} = the year the film was released`);
-*/
-
-/*
-
-*/
